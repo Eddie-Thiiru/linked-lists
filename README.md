6b2df596e059be6667f198cff4e52c0b17e33301
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-06-03 16:43:17 +0300

    Add linked-list methods

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# linked-lists
+# Linked-List
 
 From The Odin Project's curriculum.
 
-This is an implementation of a singly linked list as part of the Odin Project’s Computer Science course.
+Singly Linked List implemented with JavaScript as part of the Odin Project’s Computer Science course.
+
+# Methods
+
+- append(value) - adds a node with the given value to the end of the list.
+- prepend(value) - adds a new node with the given value to the start of the list.
+- size - returns the total number of nodes in the list
+- head - returns the first node in the list.
+- tail() - returns the last node in the list.
+- at(index) - returns the node at a given index.
+- pop() - removes the last node from the list.
+- contains(value) - returns true if the passed value is in the list otherwise return false.
+- find(value) - returns the index of the node containing passed value, or null if not found.
+- toString() - prints the Linked List as a string.
--- script.js
+++ script.js
@@ -1,61 +1,3 @@
-const LinkedList = () => {
-  let list = Node();
-
-  const append = (value) => {
-    if (list.value === null) {
-      list = { value: value };
-      list.next = null;
-    } else {
-      list.next.next = { value: value };
-      list.next.next.next = null;
-    }
-    console.log(list);
-  };
-
-  const prepend = (value) => {
-    list = { value: value, next: list };
-    console.log(list);
-  };
-
-  const size = () => {
-    let obj = list;
-    let count = 0;
-
-    function countNodes(obj) {
-      for (let key in obj) {
-        console.log(key);
-        if (typeof obj[key] === "object") {
-          return countNodes(obj[key]);
-        } else {
-          count += 1;
-        }
-      }
-    }
-    countNodes(obj);
-
-    console.log(count);
-    return count;
-  };
-
-  const head = () => {
-    console.log(list.key);
-  };
-
-  const tail = () => {};
-
-  const at = (index) => {};
-
-  const pop = () => {};
-
-  const contains = (value) => {};
-
-  const find = (value) => {};
-
-  const toString = () => {};
-
-  return { append, prepend, size, head };
-};
-
 const Node = () => {
   let list = {
     value: null,
@@ -63,6 +5,132 @@ const Node = () => {
   };
 
   return list;
+};
+
+const LinkedList = () => {
+  let list = Node();
+  let listSize = 0;
+
+  const append = (value) => {
+    if (list.value === null) {
+      list = { value: value };
+      list.next = null;
+      listSize += 1;
+    } else {
+      let current = list;
+
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = { value: value };
+      current.next.next = null;
+      listSize += 1;
+    }
+  };
+
+  const prepend = (value) => {
+    list = { value: value, next: list };
+    listSize += 1;
+  };
+
+  const size = () => {
+    return listSize;
+  };
+
+  const head = () => {
+    return list;
+  };
+
+  const tail = () => {
+    let current = list.next;
+
+    while (current.next !== null) {
+      current = current.next;
+    }
+
+    return current;
+  };
+
+  const at = (index) => {
+    let current = list;
+    let position = 0;
+
+    while (current) {
+      if (index === position) {
+        break;
+      } else {
+        current = current.next;
+      }
+      position += 1;
+    }
+
+    return current;
+  };
+
+  const pop = () => {
+    let lastNode = tail();
+    let current = list;
+
+    while (current.next !== lastNode) {
+      current = current.next;
+    }
+    current.next = null;
+    listSize--;
+  };
+
+  const contains = (value) => {
+    let current = list;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
+  };
+
+  const find = (value) => {
+    let current = list;
+    let index = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+
+      index += 1;
+      current = current.next;
+    }
+
+    return null;
+  };
+
+  const toString = () => {
+    let string = "";
+    let current = list;
+
+    while (current) {
+      string += `${current.value.toString()} next: `;
+      current = current.next;
+    }
+    string += "null";
+    console.log(string);
+  };
+
+  return {
+    append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+  };
 };
 
 const myLinkedList = LinkedList();
@@ -74,10 +142,31 @@ myLinkedList.append(100);
 myLinkedList.prepend(200);
 
 // Adds a new node to the end of the list
-myLinkedList.append(300);
+myLinkedList.append(350);
 
 // Adds a new node containing value to the start of the list
-myLinkedList.prepend(400);
+myLinkedList.prepend(600);
 
-//  Returns the total number of nodes in the list
+// Returns the total number of nodes in the list
 myLinkedList.size();
+
+// Returns the first node in the list
+myLinkedList.head();
+
+// Returns the last node in the list
+myLinkedList.tail();
+
+// Returns the node at the given index
+myLinkedList.at(1);
+
+// Removes the last node from the list
+myLinkedList.pop();
+
+// Returns true if value is in the list
+myLinkedList.contains(600);
+
+// Returns the index of the node containing value, or null if not found
+myLinkedList.find(100);
+
+// Prints list objects as a string
+myLinkedList.toString();
